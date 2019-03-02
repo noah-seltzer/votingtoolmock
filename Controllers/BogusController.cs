8f02acc624444d959665b0a2aa98db4f70e82e25
noah-seltzer <noahseltzer@outlook.com>
2019-03-02 15:46:02 -0800

    init

---
+++ Controllers/BogusController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace votingtool_mock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BogusController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet("candidates")]
+        public ActionResult<string> Get()
+        {
+            string a = @"[
+  {
+    ""candidateId"": 1,
+    ""firstName"": ""Mike"",
+    ""lastName"": ""Hansen"",
+    ""picture"": ""https://vancouver.ca/plan-your-vote/img/mayor1.jpg"",
+    ""organization"": """"
+  },
+  {
+    ""candidateId"": 2,
+    ""firstName"": ""John"",
+    ""lastName"": ""Yano"",
+    ""picture"": ""https://vancouver.ca/plan-your-vote/img/mayor2.jpg"",
+    ""organization"": """"
+  },
+  {
+    ""candidateId"": 3,
+    ""firstName"": ""David"",
+    ""lastName"": ""Chen"",
+    ""picture"": ""https://vancouver.ca/plan-your-vote/img/mayor3.jpg"",
+    ""organization"": ""ProVancouver""
+  }
+]";
+            return a ;
+        }
+
+        // GET api/values/5
+        [HttpGet("election")]
+        public ActionResult<string> Get(int id)
+        {
+            return @"{
+  ""VoteTitle"": ""BC Provincial Election"",
+  ""VoteDescription"": ""Elect members of the Legislative Assembly in the province of British Columbia"",
+  ""VoteDate"": """",
+  ""PageBackgroundColor"": ""#FFFFFF"",
+  ""PageBackgroundColorSecondary"": ""#E60050"",
+  ""PageTextColor"": ""#000000"",
+  ""LogoURL"": ""https://vancouver.ca/images/cov/feature/covlogo-share.png"",
+  ""PageFontSize"": ""1.5em"",
+  ""PageFontFamily"": ""Helvetica""
+}";
+        }
+    }
+}
